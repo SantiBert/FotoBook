31709e629ef44533d334ef8dcb39dc983c072da7
Agustin Donnetta <agu.donnetta@gmail.com>
2020-12-26 19:58:53 -0300

    otros cambios 26/12

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from .models import Category, BlogEntry, Images
 from django.views import View
+from django.views.generic.list import ListView
 
 class Galery(View):
     def get(self, request, *args, **kwargs):
+        category = Category.objects.all()
         galery = Images.objects.all()
-        context = {'galery':galery}
+        context = {
+            'galery':galery, 
+            'category':category,
+            }
         return render(request, 'gallery.html',context)
+
+class BlogEntryView(ListView):
+    def get(self, request, slug, *args, **kwargs):
+        post = BlogEntry.objects.filter(active=True)
+        category = Category.objects.get(slug)
+        categories = Category.objects.filter(active=True)
+        context = {
+            'post':post,
+            'category':category,
+            'categories':categories,
+        }
+        return render(request, 'blog.html',context)
 
 
--- fotobook/urls.py
+++ fotobook/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('galery/',include('blog.urls')),
+    path('', include('core.urls')),
 ]
 
 
