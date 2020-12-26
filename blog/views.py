6f0b358890af5792f39a0a48937c8c5f4d498146
Agustin Donnetta <agu.donnetta@gmail.com>
2020-12-26 16:25:38 -0300

    cambios 26/12

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Category, BlogEntry, Images
+from django.views import View
 
-# Create your views here.
+class Galery(View):
+    def get(self, request, *args, **kwargs):
+        galery = Images.objects.all()
+        context = {'galery':galery}
+        return render(request, 'gallery.html',context)
+
+
